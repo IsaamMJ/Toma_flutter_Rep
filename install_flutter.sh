c2d2db0ce9f14a70196d34c1f5a99addb78fdd53
IsaamMJ <136082478+IsaamMJ@users.noreply.github.com>
2025-09-08 18:29:42 +0530

    netlify setup 02

--- install_flutter.sh
+++ install_flutter.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-# Exit on error
 set -e
 
-# Install Flutter SDK
-echo "Downloading Flutter SDK..."
+echo "Installing Flutter SDK..."
 git clone https://github.com/flutter/flutter.git /opt/buildhome/flutter
 export PATH="/opt/buildhome/flutter/bin:$PATH"
 
-# Enable Flutter web
 flutter channel stable
 flutter upgrade
 flutter config --enable-web
 
-# Check Flutter version
 flutter --version
